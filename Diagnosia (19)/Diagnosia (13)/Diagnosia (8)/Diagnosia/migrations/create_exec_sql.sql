57e9b59f8b79333d34b08a7b0cf72188acd624c6
Your Name <your@email.com>
2025-02-08 12:00:00 +0300

    fix: Remove incorrectly nested project directory

--- Diagnosia (19)/Diagnosia (13)/Diagnosia (8)/Diagnosia/migrations/create_exec_sql.sql
+++
@@ -1,7 +0,0 @@
--- Create exec_sql function for running migrations
-CREATE OR REPLACE FUNCTION exec_sql(query text)
-RETURNS void AS $$
-BEGIN
-    EXECUTE query;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER; 
